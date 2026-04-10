070f90beaeadfef5f29f36e7e73a799dfc66bb4f
Michael <mikeb@360replays.com>
2026-04-09 17:30:14 -0700

    Moved destination folder root. Added appveyor builld scripts

---
+++ Binner.PrintSpoolService/BinnerPrintSpoolServiceInstaller/BinnerPrintSpoolServiceInstaller.iss
@@ -0,0 +1,230 @@
+#define MyAppName "Binner"
+#define MyAppVersion "0.0"
+#define MyAppPublisher "Binner"
+#define MyAppURL "https://github.com/replaysMike/Binner.PrintSpoolService"
+#define MyAppExeName "Binner.PrintSpoolService.exe"
+
+[Setup]
+AppId={{5B8E7506-21A8-49BB-B144-6523D0E43E34}
+AppName={#MyAppName}
+AppVersion={#MyAppVersion}
+AppVerName={#MyAppName} v{#MyAppVersion}
+AppPublisher={#MyAppPublisher}
+AppPublisherURL={#MyAppURL}
+AppSupportURL={#MyAppURL}
+AppUpdatesURL={#MyAppURL}
+VersionInfoVersion={#MyAppVersion}
+DefaultDirName={autopf}\{#MyAppName}
+DisableProgramGroupPage=yes
+LicenseFile=..\Binner.PrintSpoolService\LICENSE
+OutputDir=.\
+OutputBaseFilename=BinnerPrintSpoolServiceSetup-winx64-{#MyAppVersion}
+SetupIconFile=.\binner128x128.ico
+Compression=lzma
+SolidCompression=yes
+WizardStyle=modern
+ArchitecturesAllowed=x64
+ArchitecturesInstallIn64BitMode=x64
+UninstallDisplayIcon={app}\{#MyAppExeName}
+WizardImageFile=.\WizardLarge.bmp
+WizardSmallImageFile=.\WizardSmall.bmp
+CloseApplications=force
+UsePreviousTasks=no
+
+
+[Languages]
+Name: "english"; MessagesFile: "compiler:Default.isl"
+
+[CustomMessages]
+UninstallingService=Uninstalling existing {#MyAppName} service...
+InstallingService=Installing {#MyAppName} {#MyAppVersion} service...
+StartingApp=Starting {#MyAppName}...
+
+[Tasks]
+Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}"; Flags: unchecked
+Name: "keepconfiguration"; Description: "Keep existing configuration"
+Name: "installservice"; Description: "Install {#MyAppName} as a Windows service"
+
+[Files]
+Source: "..\Binner.PrintSpoolService\bin\Release\net10.0\win-x64\publish\*"; DestDir: "{app}"; Flags: ignoreversion recursesubdirs createallsubdirs
+
+[Icons]
+Name: "{autoprograms}\{#MyAppName}"; Filename: "{app}\{#MyAppExeName}"
+Name: "{autodesktop}\{#MyAppName}"; Filename: "{app}\{#MyAppExeName}"; Tasks: desktopicon
+
+[UninstallRun]
+Filename: "{app}\{#MyAppExeName}"; Parameters: "uninstall -servicename {#MyAppName}"; RunOnceId: "{#MyAppName}"; Flags: runascurrentuser runhidden
+
+[Code]
+type
+    SERVICE_STATUS = record
+        dwServiceType               : cardinal;
+        dwCurrentState              : cardinal;
+        dwControlsAccepted          : cardinal;
+        dwWin32ExitCode             : cardinal;
+        dwServiceSpecificExitCode   : cardinal;
+        dwCheckPoint                : cardinal;
+        dwWaitHint                  : cardinal;
+    end;
+    HANDLE = cardinal;
+const
+    SERVICE_QUERY_CONFIG        = $1;
+    SC_MANAGER_ALL_ACCESS       = $f003f;
+    SERVICE_RUNNING             = $4;
+    SERVICE_QUERY_STATUS        = $4;
+
+function OpenSCManager(lpMachineName, lpDatabaseName: string; dwDesiredAccess :cardinal): HANDLE;
+external 'OpenSCManagerW@advapi32.dll stdcall';
+
+function OpenService(hSCManager :HANDLE; lpServiceName: string; dwDesiredAccess :cardinal): HANDLE;
+external 'OpenServiceW@advapi32.dll stdcall';
+
+function CloseServiceHandle(hSCObject :HANDLE): boolean;
+external 'CloseServiceHandle@advapi32.dll stdcall';
+
+function QueryServiceStatus(hService :HANDLE;var ServiceStatus :SERVICE_STATUS) : boolean;
+external 'QueryServiceStatus@advapi32.dll stdcall';
+
+procedure CurStepChanged(CurStep: TSetupStep);
+var
+  ResultCode : Integer;
+  SourcePath: string;
+  DestPath: string;
+begin
+  // backup the appsettings
+  if CurStep = ssInstall then
+  begin
+    if WizardIsTaskSelected('keepconfiguration') then
+     begin
+      SourcePath := ExpandConstant('{app}\appsettings.json');
+      DestPath := ExpandConstant('{app}\appsettings.installerbackup.json');
+      if FileExists(SourcePath) then
+      begin
+        if not FileCopy(SourcePath, DestPath, False) then
+        begin
+         Log(Format('Backed up %s to %s', [SourcePath, DestPath]));
+        end
+          else
+        begin
+          Log(Format('Failed to Backup %s', [SourcePath]));
+        end;
+      end;
+    end;
+  end;
+  
+  // Install the service if the task was checked by the user
+  if CurStep = ssPostInstall then
+  begin
+    Log('Post install');
+
+    if WizardIsTaskSelected('keepconfiguration') then
+     begin
+      // restore the appsettings
+      SourcePath := ExpandConstant('{app}\appsettings.json');
+      DestPath := ExpandConstant('{app}\appsettings.installerbackup.json');
+      if FileExists(DestPath) then
+      begin
+        if FileCopy(DestPath, SourcePath, False) then
+        begin
+          Log(Format('Restored %s from Backup %s', [SourcePath, DestPath]));
+        end;
+      end;
+    end;
+
+    if WizardIsTaskSelected('installservice') then
+    begin
+      WizardForm.StatusLabel.Caption := CustomMessage('InstallingService');
+      WizardForm.StatusLabel.Show();
+      Log('Installing service...');
+      Exec(ExpandConstant('{app}\{#MyAppExeName}'), ExpandConstant('install --autostart'), '', SW_HIDE, ewWaitUntilTerminated, ResultCode);
+      Log(ExpandConstant('Service installed with code ' + IntToStr(ResultCode)));
+      Log('Starting service...');
+      WizardForm.StatusLabel.Caption := CustomMessage('StartingApp');
+      WizardForm.StatusLabel.Show();
+      Exec(ExpandConstant('{app}\{#MyAppExeName}'), ExpandConstant('start'), '', SW_HIDE, ewWaitUntilTerminated, ResultCode);
+      Log(ExpandConstant('Service started with code ' + IntToStr(ResultCode)));
+    end
+    else
+    begin
+      Log('Running Binner.PrintSpoolService in console');
+      WizardForm.StatusLabel.Caption := CustomMessage('StartingApp');
+      WizardForm.StatusLabel.Show();
+      Exec(ExpandConstant('{app}\{#MyAppExeName}'), '', '', SW_SHOW, ewNoWait, ResultCode);
+      Log(ExpandConstant('Application run with code ' + IntToStr(ResultCode)));
+    end;
+  end;
+end;
+
+function OpenServiceManager() : HANDLE;
+begin
+    if UsingWinNT() = true then begin
+        Result := OpenSCManager('','',SC_MANAGER_ALL_ACCESS);
+        if Result = 0 then
+            MsgBox('the servicemanager is not available', mbError, MB_OK)
+    end
+    else begin
+            MsgBox('only nt based systems support services', mbError, MB_OK)
+            Result := 0;
+    end
+end;
+
+function IsServiceInstalled(ServiceName: string) : boolean;
+var
+    hSCM    : HANDLE;
+    hService: HANDLE;
+begin
+    hSCM := OpenServiceManager();
+    Result := false;
+    if hSCM <> 0 then begin
+        hService := OpenService(hSCM, ServiceName, SERVICE_QUERY_CONFIG);
+        if hService <> 0 then begin
+            Result := true;
+            CloseServiceHandle(hService)
+        end;
+        CloseServiceHandle(hSCM)
+    end
+end;
+
+function IsServiceRunning(ServiceName: string) : boolean;
+var
+    hSCM    : HANDLE;
+    hService: HANDLE;
+    Status  : SERVICE_STATUS;
+begin
+    hSCM := OpenServiceManager();
+    Result := false;
+    if hSCM <> 0 then begin
+        hService := OpenService(hSCM, ServiceName, SERVICE_QUERY_STATUS);
+        if hService <> 0 then begin
+            if QueryServiceStatus(hService,Status) then begin
+                Result :=(Status.dwCurrentState = SERVICE_RUNNING)
+            end;
+            CloseServiceHandle(hService)
+            end;
+        CloseServiceHandle(hSCM)
+    end
+end;
+
+function PrepareToInstall(var NeedsRestart: Boolean): String;
+var
+  ResultCode : Integer;
+  ServiceInstalled : Boolean;
+  ServiceRunning : Boolean;
+begin
+  // uninstall the service if it's already installed
+  ServiceInstalled := IsServiceInstalled(ExpandConstant('{#MyAppName}'));
+  ServiceRunning := IsServiceRunning(ExpandConstant('{#MyAppName}'));
+  if ServiceInstalled then begin
+    Log('Will uninstall existing service...');
+    WizardForm.PreparingLabel.Caption := CustomMessage('UninstallingService');
+    WizardForm.PreparingLabel.Show();
+    if ServiceRunning then begin
+      Log('Stopping running service...');
+      Exec(ExpandConstant('{app}\{#MyAppExeName}'), ExpandConstant('stop'), '', SW_HIDE, ewWaitUntilTerminated, ResultCode);
+      Log(ExpandConstant('Service stopped with code ' + IntToStr(ResultCode)));
+    end;
+    Log('Uninstalling service...');
+    Exec(ExpandConstant('{app}\{#MyAppExeName}'), ExpandConstant('uninstall'), '', SW_HIDE, ewWaitUntilTerminated, ResultCode);
+    Log(ExpandConstant('Service uninstalled with code ' + IntToStr(ResultCode)));
+  end;
+end;
---
+++ Scripts/appveyor-build.ps1
@@ -0,0 +1,209 @@
+# =============================
+# Binner Appveyor Build Script
+# =============================
+$project = ".\Binner.PrintSpoolService\Binner.PrintSpoolService.slnx"
+$releaseConfiguration = "Release"
+$framework = "net10.0"
+$versionTag = "v$env:APPVEYOR_BUILD_VERSION"
+
+Write-Host "Building $env:APPVEYOR_BUILD_VERSION" -ForegroundColor magenta
+Write-Host "Build Targets: $env:BUILDTARGETS" -ForegroundColor magenta
+
+$sw = [Diagnostics.Stopwatch]::StartNew()
+  Write-Host "Installing build dependencies..." -ForegroundColor green
+  dotnet tool install --global dotnet-ef --version 9.0.2
+  choco install -y innosetup
+
+  Write-Host "Checking versions..." -ForegroundColor green
+  Write-Host "Node" -ForegroundColor cyan
+  node --version
+  Write-Host "Dotnet" -ForegroundColor cyan
+  dotnet --version
+  Write-Host "Tar" -ForegroundColor cyan
+  tar --version
+  Write-Host "Docker" -ForegroundColor cyan
+  docker --version
+
+$sw.Stop()
+$sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+
+Write-Host "Restoring packages..." -ForegroundColor green
+$sw = [Diagnostics.Stopwatch]::StartNew()
+  dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
+  dotnet restore -s https://api.nuget.org/v3/index.json $project
+  #dotnet workload update
+$sw.Stop()
+$sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+
+Write-Host "Building..." -ForegroundColor green
+$sw = [Diagnostics.Stopwatch]::StartNew()
+  dotnet build $project -c $releaseConfiguration --property WarningLevel=0
+  if ($LastExitCode -ne 0) { Write-Host "Exiting - error code '$LastExitCode'"; exit $LastExitCode }
+$sw.Stop()
+$sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+
+#Write-Host "Running tests..." -ForegroundColor green
+#$sw = [Diagnostics.Stopwatch]::StartNew()
+#  dotnet test $project -c $releaseConfiguration --no-build
+#  if ($LastExitCode -ne 0) { Write-Host "Exiting - error code '$LastExitCode'"; exit $LastExitCode }
+#$sw.Stop()
+#$sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+
+
+# publish specific profiles
+Write-Host "Publishing for each environment..." -ForegroundColor green
+
+$buildEnv = "win-x64"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
+  $sw = [Diagnostics.Stopwatch]::StartNew()
+    dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained --property WarningLevel=0
+    if ($LastExitCode -ne 0) { Write-Host "Exiting - error code '$LastExitCode'"; exit $LastExitCode }
+  $sw.Stop()
+  $sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+}
+
+$buildEnv = "linux-x64"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
+  $sw = [Diagnostics.Stopwatch]::StartNew()
+    dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained --property WarningLevel=0
+    if ($LastExitCode -ne 0) { Write-Host "Exiting - error code '$LastExitCode'"; exit $LastExitCode }
+    robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+  $sw.Stop()
+  $sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+}
+
+$buildEnv = "linux-arm"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
+  $sw = [Diagnostics.Stopwatch]::StartNew()
+    dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained --property WarningLevel=0
+    if ($LastExitCode -ne 0) { Write-Host "Exiting - error code '$LastExitCode'"; exit $LastExitCode }
+    robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+  $sw.Stop()
+  $sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+}
+
+$buildEnv = "linux-arm64"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
+  $sw = [Diagnostics.Stopwatch]::StartNew()
+    dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained --property WarningLevel=0
+    if ($LastExitCode -ne 0) { Write-Host "Exiting - error code '$LastExitCode'"; exit $LastExitCode }
+    robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+  $sw.Stop()
+  $sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+}
+
+$buildEnv = "osx-x64"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
+  $sw = [Diagnostics.Stopwatch]::StartNew()
+    dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained --property WarningLevel=0
+    if ($LastExitCode -ne 0) { Write-Host "Exiting - error code '$LastExitCode'"; exit $LastExitCode }
+    robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+  $sw.Stop()
+  $sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+}
+
+# transform configurations per environment
+# windows does not need a transform
+
+$buildEnv = "win-x64"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Copy-Item -Force -Path .\Binner.PrintSpoolService\scripts\windows\* -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish
+}
+
+$buildEnv = "linux-x64"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Move-Item -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\appsettings.Production.json -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\appsettings.default.json
+  if (!$?) { exit -1  }
+  Move-Item -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\nlog.Unix.config -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\nlog.config
+  if (!$?) { exit -1  }
+  Copy-Item -Force -Path .\Binner.PrintSpoolService\scripts\unix\* -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish
+  New-Item -ItemType Directory -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\db
+  New-Item -ItemType Directory -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\logs
+}
+
+$buildEnv = "linux-arm"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Move-Item -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\appsettings.Production.json -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\appsettings.default.json
+  if (!$?) { exit -1  }
+  Move-Item -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\nlog.Unix.config -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\nlog.config
+  if (!$?) { exit -1  }
+  Copy-Item -Force -Path .\Binner.PrintSpoolService\scripts\unix\* -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish
+  New-Item -ItemType Directory -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\db
+  New-Item -ItemType Directory -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\logs
+}
+
+$buildEnv = "linux-arm64"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Move-Item -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\appsettings.Production.json -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\appsettings.default.json
+  if (!$?) { exit -1  }
+  Move-Item -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\nlog.Unix.config -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\nlog.config
+  if (!$?) { exit -1  }
+  Copy-Item -Force -Path .\Binner.PrintSpoolService\scripts\unix\* -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish
+  New-Item -ItemType Directory -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\db
+  New-Item -ItemType Directory -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\logs
+}
+
+$buildEnv = "osx-x64"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Move-Item -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\appsettings.Production.json -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\appsettings.default.json
+  if (!$?) { exit -1  }
+  Move-Item -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\nlog.Unix.config -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\nlog.config
+  if (!$?) { exit -1  }
+  Copy-Item -Force -Path .\Binner.PrintSpoolService\scripts\unix\* -Destination .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish
+  if (!$?) { exit -1  }
+  New-Item -ItemType Directory -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\db
+  New-Item -ItemType Directory -Force -Path .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$releaseConfiguration\$framework\$buildEnv\publish\logs
+}
+
+# build installers
+$buildEnv = "win-x64"
+if ($env:BUILDTARGETS.Contains("#$buildEnv#")) {
+  Write-Host "Building Installers" -ForegroundColor green
+  $sw = [Diagnostics.Stopwatch]::StartNew()
+    Set-Location -Path .\Binner.PrintSpoolService\BinnerPrintSpoolServiceInstaller
+    (Get-Content .\BinnerPrintSpoolServiceInstaller.iss).replace('MyAppVersion "0.0"', 'MyAppVersion "' + (($env:APPVEYOR_BUILD_VERSION).split('-')[0]) + '"') | Set-Content .\BinnerPrintSpoolServiceInstaller.iss
+    (Get-Content .\BinnerPrintSpoolServiceInstaller.iss).replace('@NETFRAMEWORK@', "$framework") | Set-Content .\BinnerPrintSpoolServiceInstaller.iss
+    Write-Host "Building installer using the following script:" -ForegroundColor cyan
+    cat .\BinnerPrintSpoolServiceInstaller.iss
+    .\build-installer.cmd
+    if ($LastExitCode -ne 0) { Write-Host "Exiting - error code '$LastExitCode'"; exit $LastExitCode }
+    Set-Location -Path ..\..\
+  $sw.Stop()
+  $sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+}
+
+# package artifacts
+Write-Host "Creating artifact archives..." -ForegroundColor green
+$sw = [Diagnostics.Stopwatch]::StartNew()
+
+  # (note) windows doesn't need this, it has it's own installer that won't be archived
+  if ($env:BUILDTARGETS.Contains("#linux-x64#")) {
+    tar -czf Binner_PrintSpoolService_linux-x64.targz -C .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$($releaseConfiguration)\$framework\linux-x64\publish .
+  }
+  if ($env:BUILDTARGETS.Contains("#linux-arm#")) {
+    tar -czf Binner_PrintSpoolService_linux-arm.targz -C .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$($releaseConfiguration)\$framework\linux-arm\publish .
+  }
+  if ($env:BUILDTARGETS.Contains("#linux-arm64#")) {
+    tar -czf Binner_PrintSpoolService_linux-arm64.targz -C .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$($releaseConfiguration)\$framework\linux-arm64\publish .
+  }
+  if ($env:BUILDTARGETS.Contains("#osx-x64#")) {
+    tar -czf Binner_PrintSpoolService_osx-x64.targz -C .\Binner.PrintSpoolService\Binner.PrintSpoolService\bin\$($releaseConfiguration)\$framework\osx-x64\publish .
+  }
+$sw.Stop()
+$sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
+
+$sw = [Diagnostics.Stopwatch]::StartNew()
+  Write-Host "Uploading Artifacts" -ForegroundColor green
+  if ($env:BUILDTARGETS.Contains("#win-x64#")) {
+    Get-ChildItem .\Binner.PrintSpoolService\BinnerPrintSpoolServiceInstaller\*.exe | % { Push-AppveyorArtifact $_.FullName }
+  }
+
+  # rename these artifacts to include the build version number
+  Get-ChildItem .\*.targz -recurse | % { Push-AppveyorArtifact $_.FullName -FileName "$($_.Basename)-$env:APPVEYOR_BUILD_VERSION.tar.gz" }
+$sw.Stop()
+$sw.Elapsed | Select-Object @{n = "Elapsed"; e = { $_.Minutes, "m ", $_.Seconds, "s ", $_.Milliseconds, "ms " -join "" } }
